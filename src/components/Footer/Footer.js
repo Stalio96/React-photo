3c194459ad0ec9247263047fc316d68e6a737d47
stalio <stalio_kirkov@abv.bg>
2022-09-14 00:38:45 +0300

    Added more styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -37,6 +37,8 @@ const Footer = () => {
 
             <p className="footer__copyright">Copyright © 2022 ILIYANA'S PHOTOS. All Rights Reserved.</p>
 
+            <a className="go-to-top__link" href='#'><i class="fa-sharp fa-solid fa-arrow-up"></i></a>
+
         </footer>
     );
 }
--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,5 +1,21 @@
+import './Socials.css';
+
 const Socials = () => {
 
+    return (
+        <div className="socials">
+            <p className="socials__text">LET'S KEEP IN TOUCH !</p>
+
+            <div className="socials__container">
+                {/* <img /> */}
+                <div className="socials__insta__container">
+                    <i className="fa-brands fa-instagram"></i>
+                    <a className="socials__insta__container__instagram" href='https://www.instagram.com/iliyanaivanov/' target="_blank">@iliyanaivanov</a>
+                    <p className="socials__insta__container__text">FOLLOW ME ON INSTAGRAM</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Socials;
