8c7c9b5d035ee2af5c3dfa866e579d319ac2ea76
stalio <stalio_kirkov@abv.bg>
2022-09-13 22:17:52 +0300

    Add header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
 
+import './Header.css';
+
 const Header = () => {
 
     return (
-        <header>
-            <Link to='/'>ILIYANA'S PHOTOS</Link>
+        <header className="header">
+            <Link className="header__title" to='/'>ILIYANA'S PHOTOS</Link>
 
-            <ul>
-                <li><Link to='/'>WORK</Link></li>
-                <li><Link to='/product'>PRODUCT</Link></li>
-                <li><Link to='/about'>ABOUT</Link></li>
-                <li><Link to='/contact'>CONTACT</Link></li>
-                <li><Link to='/impressum'>IMPRESSUM</Link></li>
-                <li><Link to='/socials'>SOCIALS</Link></li>
+            <ul className="header__items">
+                <li className="header__item">
+                    <Link className="item__link" to='/'>WORK</Link>
+                </li>
+                <li className="header__item">
+                    <Link className="item__link" to='/product'>PRODUCT</Link>
+                </li>
+                <li className="header__item">
+                    <Link className="item__link" to='/about'>ABOUT</Link>
+                </li>
+                <li className="header__item">
+                    <Link className="item__link" to='/contact'>CONTACT</Link>
+                </li>
+                <li className="header__item">
+                    <Link className="item__link" to='/impressum'>IMPRESSUM</Link>
+                </li>
+                <li className="header__item">
+                    <Link className="item__link" to='/socials'>SOCIALS</Link>
+                </li>
             </ul>
         </header>
     );
