41bf82ad4380cb3655cb8b3f13b9502a558c6525
stalio <stalio_kirkov@abv.bg>
2022-09-26 15:51:52 +0300

    Add socials styles

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -4,14 +4,14 @@ const Socials = () => {
 
     return (
         <div className="socials">
-            <p className="socials__text">LET'S KEEP IN TOUCH !</p>
+            <p className="socials__text">LET`S WORK TOGETHER!</p>
 
             <div className="socials__container">
-                {/* <img /> */}
+                <img className='socials__img' src="https://scontent.fsof11-1.fna.fbcdn.net/v/t31.18172-8/26023983_10105807942169233_2758262281561815635_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=pkpb5Ut1mM0AX88WCvr&_nc_ht=scontent.fsof11-1.fna&oh=00_AT9u0pjXgpJuLBK2_lkQvDPRpmzzY3fJ0zJ_4DcXqabI2w&oe=635619EB" />
                 <div className="socials__insta__container">
                     <i className="fa-brands fa-instagram"></i>
                     <a className="socials__insta__container__instagram" href='https://www.instagram.com/iliyanaivanov/' target="_blank">@iliyanaivanov</a>
-                    <p className="socials__insta__container__text">FOLLOW ME ON INSTAGRAM</p>
+                    <p className="socials__insta__container__text">FOLLOW MY INSTAGRAM</p>
                 </div>
             </div>
         </div>
