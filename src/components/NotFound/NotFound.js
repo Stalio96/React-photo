fd218e3060cc4698559ce612663da1c5ea4bb1e9
stalio <stalio_kirkov@abv.bg>
2022-10-04 11:24:38 +0300

    Styled NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,12 +2,12 @@ import {Link} from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <section>
-            <h1>404</h1>
-            <h2>PAGE NOT FOUND</h2>
+        <section className='notFound'>
+            <h1 className='notFound__title'>404</h1>
+            <h2 className='notFound__title'>PAGE NOT FOUND</h2>
 
-            <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-            <Link to='/'>HOMEPAGE</Link>
+            <p className='notFound__text'>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+            <Link to='/' className='notFound__link'>HOMEPAGE</Link>
         </section>
     );
 }
